3be5226cab69633efe33de9a31c604c96c7a6854
Yagato <Yagatomon@gmail.com>
2022-06-17 10:48:11 -0500

    Version 1.0

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+// Yagato
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -14,7 +16,7 @@ public class Game extends JFrame {
     JPanel panel = new JPanel();
     Random random = new Random();
 
-    int startPlayer1 = 90, startPlayer2 = 90;
+    int startPlayer1Y = 180, startPlayer2Y = 180;
     int startBallX = 300, startBallY = 220;
     double speedX = 15, speedY = 15;
     int randomStartX = random.nextInt(2 - 1 + 1) + 1;
@@ -25,25 +27,24 @@ public class Game extends JFrame {
         super("Pong");
         setTitle("Pong");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.getContentPane().setSize(640, 480);
         setResizable(false);
 
         player1.setIcon(new ImageIcon("img/bar.png"));
-        player1.setBounds(10, startPlayer1, 40, 270);
+        player1.setBounds(10, startPlayer1Y, 20, 90);
 
         player2.setIcon(new ImageIcon("img/bar.png"));
-        player2.setBounds(590, startPlayer2, 40, 270);
+        player2.setBounds(610, startPlayer2Y, 20, 90);
 
         ball.setIcon(new ImageIcon("img/ball.png"));
         ball.setBounds(startBallX, startBallY, 20, 20);
 
         labelScorePlayer1.setFont(new Font("Arial", Font.BOLD, 42));
         labelScorePlayer1.setForeground(Color.white);
-        labelScorePlayer1.setBounds(280, 10, 42, 42);
+        labelScorePlayer1.setBounds(280, 10, 100, 42);
 
         labelScorePlayer2.setFont(new Font("Arial", Font.BOLD, 42));
         labelScorePlayer2.setForeground(Color.white);
-        labelScorePlayer2.setBounds(340, 10, 42, 42);
+        labelScorePlayer2.setBounds(340, 10, 100, 42);
 
         panel.setLayout(null);
         panel.setPreferredSize(new Dimension(640, 480));
@@ -62,27 +63,29 @@ public class Game extends JFrame {
         setVisible(true);
         setLocationRelativeTo(null);
 
-        if(randomStartX == 1)
-            speedX *= -1;
-        if(randomStartY == 1)
-            speedY *= -1;
-
+        randomDirection();
         ballMovement();
-
         wincon();
     }
 
     public void ballMovement(){
         Thread thread = new Thread(() -> {
             while(true) {
-                if(startBallX >= 620) {
+                if(intersects(player1, ball))
                     speedX = -speedX;
-                    scorePlayer1 += 5;
+                else if(intersects(player2, ball))
+                    speedX *= -1;
+
+                if(startBallX >= 620) {
+                    startBallX = 300;
+                    startBallY = 220;
+                    scorePlayer1 += 1;
                     labelScorePlayer1.setText(Integer.toString(scorePlayer1));
                 }
                 else if(startBallX <= 0) {
-                    speedX *= -1;
-                    scorePlayer2 += 5;
+                    startBallX = 300;
+                    startBallY = 220;
+                    scorePlayer2 += 1;
                     labelScorePlayer2.setText(Integer.toString(scorePlayer2));
                 }
 
@@ -105,18 +108,35 @@ public class Game extends JFrame {
         thread.start();
     }
 
+    public void randomDirection(){
+        if(randomStartX == 1)
+            speedX *= -1;
+        if(randomStartY == 1)
+            speedY *= -1;
+    }
+
+    public boolean intersects(JLabel player, JLabel ball){
+        Rectangle ballBounds = ball.getBounds();
+
+        Rectangle result = SwingUtilities.computeIntersection(player.getX(), player.getY(), player.getWidth(),
+                player.getHeight(), ballBounds);
+
+        return (result.getWidth() > 0 && result.getHeight() > 0);
+    }
+
     public void wincon(){
         Thread thread = new Thread(() -> {
             while(true) {
                 if (scorePlayer1 == 5) {
                     JOptionPane.showMessageDialog(this, "Player 1 won");
                     restart();
-                } else if (scorePlayer2 == 5) {
+                }
+                else if (scorePlayer2 == 5) {
                     JOptionPane.showMessageDialog(this, "Player 2 won");
                     restart();
                 }
                 try{
-                    Thread.sleep(10);
+                    Thread.sleep(100);
                 }catch(Exception e){
                     e.printStackTrace();
                 }
@@ -130,12 +150,13 @@ public class Game extends JFrame {
         scorePlayer2 = 0;
         startBallX = 300;
         startBallY = 220;
-        startPlayer1 = 90;
-        startPlayer2 = 90;
+        startPlayer1Y = 180;
+        startPlayer2Y = 180;
         randomStartX = random.nextInt(2 - 1 + 1) + 1;
         randomStartY = random.nextInt(2 - 1 + 1) + 1;
-        player1.setBounds(10, startPlayer1, 40, 270);
-        player2.setBounds(590, startPlayer2, 40, 270);
+        randomDirection();
+        player1.setBounds(10, startPlayer1Y, 20, 90);
+        player2.setBounds(610, startPlayer2Y, 20, 90);
         ball.setBounds(startBallX, startBallY, 20, 20);
         labelScorePlayer1.setText("0");
         labelScorePlayer2.setText("0");
@@ -150,20 +171,20 @@ public class Game extends JFrame {
         @Override
         public void keyPressed(KeyEvent e) {
             if (e.getKeyCode() == KeyEvent.VK_UP) {
-                startPlayer1 -= 10;
-                player1.setLocation(10, startPlayer1);
+                startPlayer1Y -= 10;
+                player1.setLocation(10, startPlayer1Y);
             }
             else if(e.getKeyCode() == KeyEvent.VK_DOWN){
-                startPlayer1 += 10;
-                player1.setLocation(10, startPlayer1);
+                startPlayer1Y += 10;
+                player1.setLocation(10, startPlayer1Y);
             }
             else if(e.getKeyCode() == KeyEvent.VK_W){
-                startPlayer2 -= 10;
-                player2.setLocation(575, startPlayer2);
+                startPlayer2Y -= 10;
+                player2.setLocation(610, startPlayer2Y);
             }
             else if(e.getKeyCode() == KeyEvent.VK_S){
-                startPlayer2 += 10;
-                player2.setLocation(575, startPlayer2);
+                startPlayer2Y += 10;
+                player2.setLocation(610, startPlayer2Y);
             }
         }
 
